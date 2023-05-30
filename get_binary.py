cd0f51351af6154a9b5d627ed6abf07f9a1ef01d
sergiopon <131701295+sergiopon@users.noreply.github.com>
2023-05-30 08:44:36 -0500

    Update get_binary.py

--- get_binary.py
+++ get_binary.py
@@ -1,5 +1,17 @@
 def get_binary(ascii_code):
+    """
+    Convierte un código ASCII en su representación binaria.
 
+    Parameters:
+        ascii_code (int): El código ASCII a convertir.
+
+    Returns:
+        str: La representación binaria del código ASCII.
+
+    Example:
+        >> get_binary(65)
+        '1000001'
+    """
     binario = ""
     while ascii_code > 0:
         residuo = ascii_code % 2
