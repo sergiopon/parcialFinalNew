ced843cae56c98f33f8c0d9d1314ac701ab588ad
sergiopon <131701295+sergiopon@users.noreply.github.com>
2023-05-30 08:46:36 -0500

    se documenta main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from get_ascii import get_ascii
 from get_binary import get_binary
 def get_result(palabra):
+    """
+    Obtiene el resultado de procesar cada caracter de una palabra.
 
+    Parameters:
+        palabra (str): La palabra a procesar.
+
+    Returns:
+        list: Una lista de tuplas que contienen información sobre cada caracter procesado.
+
+    Example:
+        >> get_result("Hello")
+        [('El caracter es:', 'H', ', El código Ascii es: ', 72, ', El binario es:', '1001000'),
+         ('El caracter es:', 'e', ', El código Ascii es: ', 101, ', El binario es:', '1100101'),
+         ('El caracter es:', 'l', ', El código Ascii es: ', 108, ', El binario es:', '1101100'),
+         ('El caracter es:', 'l', ', El código Ascii es: ', 108, ', El binario es:', '1101100'),
+         ('El caracter es:', 'o', ', El código Ascii es: ', 111, ', El binario es:', '1101111')]
+    """
     resultado = []
     for caracter in palabra:
         ascii_code = get_ascii(caracter)
